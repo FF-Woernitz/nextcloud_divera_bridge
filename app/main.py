95e113ce250455b0c8f5d286c1b4da84067679c0
TobsA <git@tobsa.wtf>
2024-04-07 22:05:30 +0200

    Remove group logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import secrets
+from pprint import pprint
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -7,8 +8,9 @@ from fastapi.responses import RedirectResponse
 
 import requests
 import logging
-
-CLUSTER_GROUPS = {5649: [22697, 138070], 9685: [138069]}
+# 5649 = FFW
+# 9685 = FFW Test
+CLUSTERS = [5649, 9685]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -59,17 +61,10 @@ def login_divera(credentials: Annotated[HTTPBasicCredentials, Depends(security)]
         clusters[v["cluster_id"]] = k
 
     for cluster, ucr in clusters.items():
-        if cluster in CLUSTER_GROUPS.keys():
-            resp_ucr = requests.get('https://www.divera247.com/api/v2/pull/all',
-                                    params={"accesskey": accesskey, "ucr": ucr})
-            resp_ucr = resp_ucr.json()
-            if not resp_ucr["success"]:
-                continue
-            for g in CLUSTER_GROUPS[cluster]:
-                if g in resp_ucr["data"]["cluster"]["consumer"][ucr]["groups"]:
-                    return {"username": credentials.username, "cluster": cluster, "ucr": ucr}
+        if cluster in CLUSTERS:
+            return {"username": credentials.username, "cluster": cluster, "ucr": ucr}
 
-    return_error("Divera group failed")
+    return_error("Divera cluster failed")
 
 
 @app.get("/login")
